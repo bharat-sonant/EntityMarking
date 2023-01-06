6ef54cb8433900499bbbe22dae582836c108dc61
Wevois <Pradeep.wevois22@gmail.com>
2023-01-06 13:11:33 +0530

    code commit

--- Code/app/src/main/java/com/example/entitymarking/MainActivity.java
+++ Code/app/src/main/java/com/example/entitymarking/MainActivity.java
@@ -55,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
                 return;
             }
         }
-        setDatabase("Tonk");
+        setDatabase("Bhiwadi");
     }
 
     @SuppressLint("StaticFieldLeak")
